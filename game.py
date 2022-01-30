7a7cc1f81f97b57b1e705450e3af966f9c1eba87
ARM <alexander.michaud@aya.yale.edu>
2022-01-30 12:49:46 -0500

    Make class constants

--- game.py
+++ game.py
@@ -1,10 +1,11 @@
 import random
 
-INCORRECT_LETTER = 0
-INCORRECT_PLACEMENT = 1
-CORRECT_LETTER = 2
-
 class Game:
+
+    INCORRECT_LETTER = 0
+    INCORRECT_PLACEMENT = 1
+    CORRECT_LETTER = 2
+
     def __init__(self, word_bank: list) -> None:
         self.answer = random.choice(word_bank)
         self.guessed_words = [None] * 6
@@ -16,7 +17,7 @@ class Game:
         game_won = self.answer == guess
         if game_won or self.guesses == 6:
             self.__save_game()
-        return [CORRECT_LETTER] * 5 if game_won else self.__evaluate_guess(guess)
+        return [self.CORRECT_LETTER] * 5 if game_won else self.__evaluate_guess(guess)
 
     def __evaluate_guess(self, guess: str) -> list:
         ans = self.answer
@@ -24,18 +25,18 @@ class Game:
 
         for i, letter in enumerate(guess):
             if ans[i] == letter:
-                result[i] = CORRECT_LETTER
+                result[i] = self.CORRECT_LETTER
                 guess = guess[:i] + '_' + guess[i+1:]
                 ans = ans[:i] + '_' + ans[i+1:]
 
         for i, letter in enumerate(guess):
             if letter != '_' and letter in ans:
-                result[i] = INCORRECT_PLACEMENT 
+                result[i] = self.INCORRECT_PLACEMENT 
                 guess = guess[:i] + '_' + guess[i+1:]
                 ans[ans.find(letter)] = '_'
                 
         if not all(result):
-            return list(map(lambda x: INCORRECT_LETTER if x is None else x, result))
+            return list(map(lambda x: self.INCORRECT_LETTER if x is None else x, result))
 
     def __save_game(self):
         # TODO Save to DB
